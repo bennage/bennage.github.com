ef134060df828f3a05e03d12a2a4d45245a42d31
Christopher Bennage <christopher@bennage.com>
2012-11-17 19:06:07 -0800

    more customizations

--- source/javascripts/newhope.js
+++ source/javascripts/newhope.js
@@ -1,18 +1,23 @@
 (function() {
 
 	// http://blogs.sitepointstatic.com/examples/tech/css3-starwars/index.html
+	var button_html = '<li id="newhope" title="switch to Star Wards mode"><a href="#"><span>Star Wars Mode</span></a></li>';
+
 	$.domReady(function() {
 
-		var nav = $('.main-navigation').append('<li><a href="#" class="newhope"><img src="/images/R2D2-icon.png"/></a></li>');
-		nav.children().last().find('a').click(function() {
-			$('body').hide();
+		var body = $('body');
+		$('.main-navigation').append(button_html);
+
+		$('#newhope').find('a').click(function() {
+			body.hide();
 			$('[type="text/css"]').remove();
 			$('nav').hide();
 			$('footer').hide();
 			$('aside').hide();
 			$('head').append('<link href="/stylesheets/newhope.css" media="screen, projection" rel="stylesheet" type="text/css">');
+
 			setTimeout(function() {
-				$('body').show();
+				body.show();
 			}, 300);
 		});
 
